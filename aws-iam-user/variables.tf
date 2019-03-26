dd867027136e9940a8aee09dc9f0a746cdae73e7
John Pena <jpena@jpena-mbp.local>
2019-03-25 20:01:26 -0700

    Add aws-iam-user module

---
+++ aws-iam-user/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_name" {}
+variable "aws_pgp_key" {}
+variable "aws_groups" {
+  type = "list"
+  default = []
+}
+variable "aws_force_destroy" {
+  default = "true"
+}
+variable "aws_password_reset_required" {
+  default = "false"
+}
+variable "aws_user_enabled" {
+  default = "true"
+}
+variable "aws_login_profile_enabled" {
+  default = "true"
+}
