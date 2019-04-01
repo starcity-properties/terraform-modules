8972cb940c2c77e8688e6539774a91f903406e5c
John Pena <jpena@jpena-mbp.local>
2019-04-01 10:46:41 -0700

    Add initial module config for cloudfront cdn with s3 origin

---
+++ aws-cloudfront-s3-cdn/s3-bucket.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket" "bucket" {
+
+  bucket = "${var.s3_bucket_name}"
+  acl    = "${var.s3_bucket_acl}"
+  region = "${var.s3_bucket_region}"
+
+}
---
+++ aws-cloudfront-s3-cdn/variables.tf
@@ -0,0 +1,14 @@
+# origin variables
+variable "cloudfront_origin" {}
+variable "cloudfront_enabled" {
+  default = "true"
+}
+
+variable "cloudfront_comment" {}
+variable "cloudfront_ipv6_enabled" {}
+variable "cloudfront_aliases" {}
+
+# s3 variables
+variable "s3_bucket_name" {}
+variable "s3_bucket_acl" {}
+variable "s3_bucket_region" {}
