a6c66a1f905ff9f755666438378a173d075ae824
John Pena <jpena@jpena-mbp.local>
2019-04-16 22:02:51 -0700

    Add security group rules to bastion module

--- aws-bastion/bastion.tf
+++ aws-bastion/bastion.tf
@@ -1,5 +1,23 @@
-resource "aws_instance" "ec2" {
+resource "aws_instance" "bastion" {
   ami = "${var.ami}"
   instance_type = "${var.instance_type}"
   subnet_id = "${var.subnet_id}"
+}
+
+resource "aws_security_group" "bastion-sg" {
+  description = "Allow SSH access to bastion host from specific source IPs"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["199.188.195.215/32", "73.15.166.45/32", "104.193.169.148/32", "41.90.125.50/32"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
 }
