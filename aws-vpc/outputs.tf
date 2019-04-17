727f330998933364a6777e736f17fe1dd08259c0
John Pena <jpena@jpena-mbp.local>
2019-04-16 22:22:11 -0700

    Add additional VPC output needed for other module

--- aws-vpc/outputs.tf
+++ aws-vpc/outputs.tf
@@ -7,3 +7,8 @@ output "public_subnet_id" {
   description = "List of id's of public subnets"
   value  = ["${aws_subnet.public-subnet.*.id}"]
 }
+
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value = "${aws_vpc.vpc.id}"
+}
