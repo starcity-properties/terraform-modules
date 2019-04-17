f7313cfc49d4d69e1772d45358eae513a9e23f52
John Pena <jpena@jpena-mbp.local>
2019-04-17 11:38:25 -0700

    Add private subnet output to aws-vpc

--- aws-vpc/outputs.tf
+++ aws-vpc/outputs.tf
@@ -8,6 +8,11 @@ output "public_subnet_id" {
   value       = ["${aws_subnet.public-subnet.*.id}"]
 }
 
+output "private_subnet_id" {
+  description = "List of id's of private subnets"
+  value       = ["${aws_subnet.private-subnet.*.id}"]
+}
+
 output "vpc_id" {
   description = "ID of the created VPC"
   value       = "${aws_vpc.vpc.id}"
