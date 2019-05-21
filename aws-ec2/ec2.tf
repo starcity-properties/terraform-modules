6fc058457ab7494193f616b3a770ff0b40295cfe
John Pena <jpena@jpena-mbp.local>
2019-05-21 15:55:25 -0700

    Add few more var blocks

--- aws-ec2/ec2.tf
+++ aws-ec2/ec2.tf
@@ -1,4 +1,12 @@
 resource aws_instance ec2 {
-  ami           = "${var.aws_ami}"
-  instance_type = "${var.aws_instance_type}"
+  ami                    = "${var.ami}"
+  instance_type          = "${var.instance_type}"
+  iam_instance_profile   = ""
+  subnet_id              = "${var.subnet_id}"
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ""
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
--- aws-ec2/variables.tf
+++ aws-ec2/variables.tf
@@ -1,7 +1,19 @@
-variable "aws_ami" {
+variable "ami" {
   description = "AWS AMI ID for your EC2 instance"
 }
 
-variable "aws_instance_type" {
+variable "instance_type" {
   description = "The EC2 instance type"
+}
+
+variable "key_name" {
+  description = "Key pair used for your EC2 instance"
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet you want to deploy your EC2 instance"
+}
+
+variable "iam_instance_profile" {
+  description = "The IAM Instance Profile to launch the instance with"
 }
