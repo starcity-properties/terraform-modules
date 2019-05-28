97d0dee390797dd0aa923fc67ee16a08b622e877
John Pena <jpena@jpena-mbp.local>
2019-05-28 14:53:23 -0700

    Update EC2 modules

--- aws-ec2/ec2.tf
+++ aws-ec2/ec2.tf
@@ -1,16 +1,60 @@
-resource aws_instance ec2 {
+# EC2 Instance
+resource aws_instance aws-ec2 {
   ami                    = "${var.ami}"
   instance_type          = "${var.instance_type}"
-  iam_instance_profile   = ""
-  subnet_id              = "${var.public_subnet_id}"
+  iam_instance_profile   = "${var.iam_instance_profile}"
+  subnet_id              = "${element(var.public_subnet_id, 0)}"
   key_name               = "${var.key_name}"
-  vpc_security_group_ids = ""
+  vpc_security_group_ids = ["${aws_security_group.aws-sg.id}"]
+
+  root_block_device {
+    volume_size = "${var.volume_size}"
+    volume_type = "${var.volume_type}"
+  }
 
   lifecycle {
     create_before_destroy = true
   }
 
   tags {
-    Name = "${var.ec2_name}"
+    Name = "${var.application}-${var.environment}"
   }
+}
+
+# EC2 Security Group
+resource "aws_security_group" "aws-sg" {
+  name        = "${var.application}-sg-${var.environment}"
+  vpc_id      = "${var.vpc_id}"
+  description = "Security group that allows incoming traffic from Bastion and ALB only"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["${var.ssh_cidr}"]
+  }
+
+  ingress {
+    from_port   = "${var.ingress_port}"
+    to_port     = "${var.ingress_port}"
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.application}-sg-${var.environment}"
+  }
+}
+
+# Elastic IP
+resource "aws_eip" "aws-eip" {
+  instance = "${aws_instance.aws-ec2.id}"
+  vpc      = true
 }
--- aws-ec2/variables.tf
+++ aws-ec2/variables.tf
@@ -1,3 +1,11 @@
+variable "environment" {
+  description = "AWS environment"
+}
+
+variable "application" {
+  description = "The name of your application"
+}
+
 variable "ami" {
   description = "AWS AMI ID for your EC2 instance"
 }
@@ -11,25 +19,31 @@ variable "key_name" {
 }
 
 variable "public_subnet_id" {
+  type        = "list"
   description = "The ID of the subnet you want to deploy your EC2 instance"
 }
 
+variable "vpc_id" {
+  description = "VPC ID"
+}
+
+variable "ssh_cidr" {
+  type        = "list"
+  description = "CIDR block for bastion access"
+}
+
+variable "volume_type" {
+  description = "The volume type for EC2"
+}
+
+variable "volume_size" {
+  description = "The volume size for EC2"
+}
+
+variable "ingress_port" {
+  description = "The ingress port for the SG"
+}
+
 variable "iam_instance_profile" {
-  description = "The IAM Instance Profile to launch the instance with"
-}
-
-variable "ec2_name" {
-  description = "Name tag of your EC2 instance"
-}
-
-variable "sg_name" {
-  description = "Name tag of your SG"
-}
-
-variable "cidrs" {
-  description = "SG cidr blocks"
-}
-
-variable "tcp_ports" {
-  description = "TCP ports for SG"
+  description = "The instance profile used for your EC2 instance"
 }
