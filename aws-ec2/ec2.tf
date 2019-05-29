908d28ce505d63a5920701b3563bf458b6333b8c
John Pena <jpena@jpena-mbp.local>
2019-05-29 11:14:03 -0700

    Add port 443 and 80 to SG

--- aws-ec2/ec2.tf
+++ aws-ec2/ec2.tf
@@ -41,6 +41,20 @@ resource "aws_security_group" "aws-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
