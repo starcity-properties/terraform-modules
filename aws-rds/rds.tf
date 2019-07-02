212f12b328ea37570a573113e11c4cb286214ebe
John Pena <jpena@starcity.com>
2019-07-02 15:59:08 -0700

    Remove references to Billboard

--- aws-rds/rds.tf
+++ aws-rds/rds.tf
@@ -10,26 +10,26 @@ resource "aws_db_instance" "rds" {
   username               = "${var.username}"
   password               = "${var.password}"
   db_subnet_group_name   = "${aws_db_subnet_group.rds-subnet-group.name}"
-  vpc_security_group_ids = ["${aws_security_group.billboard-rds-sg.id}"]
+  vpc_security_group_ids = ["${aws_security_group.rds-sg.id}"]
   skip_final_snapshot    = true
   deletion_protection    = true
 }
 
 # RDS Subnet Group
 resource "aws_db_subnet_group" "rds-subnet-group" {
-  name       = "billboard-rds-subnet-group-${var.environment}"
+  name       = "${var.application}-rds-subnet-group-${var.environment}"
   subnet_ids = ["${var.private_subnet_id}"]
 
   tags = {
-    Name = "billboard-rds-subnet-group-${var.environment}"
+    Name = "${var.application}-rds-subnet-group-${var.environment}"
   }
 }
 
 # RDS Security Group
-resource "aws_security_group" "billboard-rds-sg" {
-  name        = "billboard-rds-sg-${var.environment}"
+resource "aws_security_group" "rds-sg" {
+  name        = "${var.application}-rds-sg-${var.environment}"
   vpc_id      = "${var.vpc_id}"
-  description = "Security group that allows only Billboard instances to access RDS"
+  description = "Security group that allows only the specified EC2 instance to access RDS"
 
   ingress {
     from_port       = "${var.db_port}"
