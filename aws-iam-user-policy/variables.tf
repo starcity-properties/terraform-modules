1511033b0031780a7dca5917b33cb371f437fd0f
John Pena <jpena@starcity.com>
2019-09-04 16:41:38 -0700

    Add missing variables to aws-iam-user-policy

--- aws-iam-user-policy/variables.tf
+++ aws-iam-user-policy/variables.tf
@@ -1,4 +1,11 @@
 variable "user" {
-  type        = string
   description = "The user to be associated with the policy"
+}
+
+variable "name" {
+  description = "The name given to the policy"
+}
+
+variable "policy" {
+  description = "The policy that is applied to the user"
 }
