d5b462f5ce2c2ee92f9d8c2ee616670a55b40604
Diana Negma <dianagren@gmail.com>
2019-09-06 13:08:08 -0700

    Add change password policy. (#4)

--- aws-iam-group/iam-policy-attachment.tf
+++ aws-iam-group/iam-policy-attachment.tf
@@ -3,3 +3,7 @@ resource "aws_iam_group_policy_attachment" "policy" {
   policy_arn = var.aws_policy
 }
 
+resource "aws_iam_group_policy_attachment" "change_password" {
+  group      = aws_iam_group.group.name
+  policy_arn = var.aws_change_password_policy
+}
--- aws-iam-group/variables.tf
+++ aws-iam-group/variables.tf
@@ -2,3 +2,6 @@ variable "aws_group_name" {}
 variable "aws_group_path" {}
 variable "aws_policy" {}
 
+variable "aws_change_password_policy" {
+  default = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+}
