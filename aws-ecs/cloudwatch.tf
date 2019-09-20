275f831b17fe3b2b9c893fe7ce60334f7e957b9c
Diana Negma <dianagren@gmail.com>
2019-09-19 17:42:05 -0700

    Generalize the notification pattern.

--- aws-ecs/cloudwatch.tf
+++ aws-ecs/cloudwatch.tf
@@ -1,13 +1,29 @@
-data "template_file" "event_pattern" {
-  template = file("${path.module}/files/deploy-failed-notification.json")
+data "template_file" "event_pattern_stopped" {
+  template = file("${path.module}/files/deploy-notification.json")
 
   vars = {
     cluster_arn = aws_ecs_cluster.ecs_cluster.id
+    last_status = "STOPPED"
   }
 }
 
 resource "aws_cloudwatch_event_rule" "failed-deploy" {
   name          = "failed-deploy-${var.application}-${var.environment}"
-  description   = "Deploy failure notificatioon ${var.application}-${var.environment}"
-  event_pattern = data.template_file.event_pattern.rendered
+  description   = "Deploy failure notification ${var.application}-${var.environment}"
+  event_pattern = data.template_file.event_pattern_stopped.rendered
+}
+
+data "template_file" "event_pattern_stopped" {
+  template = file("${path.module}/files/deploy-notification.json")
+
+  vars = {
+    cluster_arn = aws_ecs_cluster.ecs_cluster.id
+    last_status = "RUNNING"
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "failed-deploy" {
+  name          = "running-${var.application}-${var.environment}"
+  description   = "Deploy running notification ${var.application}-${var.environment}"
+  event_pattern = data.template_file.event_pattern_stopped.rendered
 }
