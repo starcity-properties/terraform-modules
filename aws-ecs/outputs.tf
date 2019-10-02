8d956ed01d110edf59908059dc1861c9887ae529
Diana Negma <dianagren@gmail.com>
2019-10-02 10:19:22 -0700

    Add outputs to IAM and ECS modules. (#12)

--- aws-ecs/outputs.tf
+++ aws-ecs/outputs.tf
@@ -3,3 +3,10 @@ output "sg_id" {
   value       = aws_security_group.alb-sg.id
 }
 
+output "cloudwatch_event_rule_running_ecs_name" {
+  value = aws_cloudwatch_event_rule.running-deploy.name
+}
+
+output "cloudwatch_event_rule_failed_ecs_name" {
+  value = aws_cloudwatch_event_rule.failed-deploy.name
+}
--- aws-iam/role/outputs.tf
+++ aws-iam/role/outputs.tf
@@ -2,3 +2,8 @@ output "iam_role_name" {
   value       = aws_iam_role.role.name
   description = "The role name."
 }
+
+output "iam_role_arn" {
+  value       = aws_iam_role.role.arn
+  description = "The role ARN."
+}
